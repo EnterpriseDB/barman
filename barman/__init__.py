1dbd2b0499d0c5d3047c67e964258e545d257de7
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2011-08-30 11:03:58 +0200

    Move configuration handling in pre_call hook

--- barman/__init__.py
+++ barman/__init__.py
@@ -17,3 +17,5 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from version import __version__
+
+__config__ = None
--- barman/cli.py
+++ barman/cli.py
@@ -16,13 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from argh import ArghParser, alias, arg
+import barman.config
 
-def list():
+def list(args):
     "list available servers, with useful information"
     pass
 
-def cron():
+def cron(args):
     "run maintenance tasks"
     pass
 
@@ -31,35 +33,30 @@ all server commands require a server-name argument
 """
 
 @alias('backup')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('server-name', help='specifies the server name for the command')
 def server_backup(args):
     'perform a full backup for the given server'
     pass
 
 @alias('list')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('server-name', help='specifies the server name for the command')
 def server_list(args):
     'list available backups for the given server'
     pass
 
 @alias('status')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('server-name', help='specifies the server name for the command')
 def server_status(args):
     'shows live information and status of the PostgreSQL server'
     pass
 
 @alias('delete_obsolete')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('server-name', help='specifies the server name for the command')
 def server_delete_obsolete(args):
     'delete obsolete backups and WAL (according to retention policy)'
     pass
 
 @alias('recover')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('server-name', help='specifies the server name for the command')
 @arg('--target-time', help='target time')
 @arg('--target-xid', help='target xid')
@@ -73,36 +70,43 @@ all backup commands accept require a backup-id argument
 """
 
 @alias('show')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('backup-id', help='specifies the backup ID')
 def backup_show(args):
     'show a single backup information'
     pass
 
 @alias('terminate')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('backup-id', help='specifies the backup ID')
 def backup_terminate(backup):
     'terminate a running backup'
     pass
 
 @alias('delete')
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('backup-id', help='specifies the backup ID')
 def backup_delete(backup):
     'delete a backup'
     pass
 
-@arg('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
 @arg('backup-id', help='specifies the backup ID')
 @alias('recover')
 def backup_recover(backup):
     'recover a backup'
     pass
 
+def load_config(args):
+    if hasattr(args, 'config'):
+        barman.__config__ = barman.config.Config(args.config)
+    else:
+        try:
+            file = os.environ['BARMAN_CONFIG_FILE']
+        except KeyError:
+            file = None
+        barman.__config__ = barman.config.Config(file)
+
 def main():
     p = ArghParser()
     p.add_argument('-v', '--version', action='version', version=barman.__version__)
+    p.add_argument('-c', '--config', help='uses a configuration file (defaults: $HOME/.barman.conf, /etc/barman.conf)')
     p.add_commands([list, cron])
     p.add_commands(
         [
@@ -127,7 +131,7 @@ def main():
         title='commands acting on a backup',
         description=BACKUP_DESCRIPTION,
     )
-    p.dispatch()
+    p.dispatch(pre_call=load_config)
 
 if __name__ == '__main__':
     main()
