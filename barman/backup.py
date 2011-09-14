8375b8c335ed59cc3306d032ba257fc56e50ab69
Gabriele Bartolini <gabriele.bartolini@2ndQuadrant.it>
2011-09-14 19:27:03 +0200

    Add comments to backup.py and small improvements
    Added documentation and comments.
    Added an exception when parsing backup.info malformed lines.

--- barman/backup.py
+++ barman/backup.py
@@ -21,52 +21,80 @@ import os
 import re
 
 class Backup(object):
+    """
+    PostgreSQL Physical base backup
+    """
 
     KEYS = [ 'version', 'pgdata', 'tablespaces', 'timeline',
              'begin_time', 'begin_xlog', 'begin_wal', 'begin_offset',
              'end_time', 'end_xlog', 'end_wal', 'end_offset',
              'status',
     ]
+    """
+    Attributes of the backup.info file
+    """
 
     WAL_re = re.compile(r'([\dA-Fa-f]{8})([\dA-Fa-f]{8})([\dA-Fa-f]{8})')
+    """
+    WAL file segment name parser (regular expression)
+    """
 
+    # Taken from xlog_internal.h from PostgreSQL sources
     XLOG_SEG_SIZE = 1 << 24
     XLOG_SEG_PER_FILE = 0xffffffff / XLOG_SEG_SIZE
     XLOG_FILE_SIZE = XLOG_SEG_SIZE * XLOG_SEG_PER_FILE
 
     def __init__(self, file=None, backup_id=None):
+        """
+        Constructor
+        """
+        # Initialises the attributes for the object based on the predefined keys
         self.__dict__.update(dict.fromkeys(self.KEYS))
+        self.backup_id = backup_id
         if file:
-            if hasattr(file, 'read'):
+            # Looks for a backup.info file
+            if hasattr(file, 'read'): # We have been given a file-like object
                 info = file
                 filename = os.path.abspath(file.name)
-            else:
+            else: # Just a file name
                 filename = os.path.abspath(file)
                 info = open(file, 'r').readlines()
-            if not backup_id:
+            if not backup_id: # Detect the backup ID from the name of the parent dir
                 self.backup_id = os.path.basename(os.path.dirname(filename))
-            else:
-                self.backup_id = backup_id
+            # Parses the backup.info file
             for line in info:
                 try:
                     key, value = line.rstrip().split('=')
                 except:
-                    continue
+                    raise Exception('invalid line in backup file: %s' % line)
                 if key not in Backup.KEYS:
                     raise Exception('invalid key in backup file: %s' % key)
-                if key == 'tablespaces':
+                if key == 'tablespaces': # Treat the tablespaces as a literal Python list of tuples
                     self.__dict__[key] = ast.literal_eval(value)
                 else:
                     self.__dict__[key] = value
 
+    def _decode_segment_name(self, name):
+        """
+        Retrieve the timeline, log ID and segment ID from the name of the WAL segment
+        """
+        return [int(x, 16) for x in self.WAL_re.match(name).groups()]
+    
     def _segment_name(self, tli, log, seg):
+        """
+        Build the WAL segment name based on timeline, log ID and segment ID
+        """
         return "%08X%08X%08X" % (tli, log, seg)
 
     def get_required_wal_segments(self):
-        begin_tli, begin_log, begin_seg = [int(x, 16) for x in self.WAL_re.match(self.begin_wal).groups()]
-        end_tli, end_log, end_seg = [int(x, 16) for x in self.WAL_re.match(self.end_wal).groups()]
-        assert begin_tli == end_tli
+        """
+        Get the list of required WAL segments for the current backup
+        """
+        begin_tli, begin_log, begin_seg = self._decode_segment_name(self.begin_wal)
+        end_tli, end_log, end_seg = self._decode_segment_name(self.end_wal)
+        assert begin_tli == end_tli # Check for timeline equality
 
+        # Start from the first WAL and sequentially enumerates the segments to the end
         cur_log, cur_seg = begin_log, begin_seg
         while cur_log < end_log or cur_seg <= end_seg:
             yield self._segment_name(begin_tli, cur_log, cur_seg)
