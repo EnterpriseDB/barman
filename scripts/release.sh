00fd20f15d886fde180948d6717045c6b6889c45
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2011-10-17 11:59:01 +0200

    Added a script to generate the Changelog and sign the release tag

---
+++ scripts/release.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+BASE="$(dirname $(cd $(dirname "$0"); pwd))"
+cd "$BASE"
+
+VERSION="$(python -c 'd={}; execfile("barman/version.py", d); print d["__version__"]')"
+scripts/gitlog-to-changelog > ChangeLog
+git add ChangeLog
+git commit -m "Update the ChangeLog file" 
+scripts/gitlog-to-changelog > ChangeLog
+git add ChangeLog
+git commit -m "Update the ChangeLog file" --amend
+./setup.py sdist
+if ! git tag -s -m "Release ${VERSION}" release/${VERSION}
+then
+  echo "Cannot tag the release as the private key is missing"
+fi
