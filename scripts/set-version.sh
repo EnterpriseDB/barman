e9633eab01283435164237e4d16d2f1062a38ea5
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2019-03-20 11:18:38 +0100

    Add utility script to set a new barman version
    Signed-off-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>

---
+++ scripts/set-version.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Copyright (C) 2019 2ndQuadrant Limited
+#
+# This file is part of Barman.
+#
+# Barman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Barman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Barman.  If not, see <http://www.gnu.org/licenses/>.
+
+set -eu
+
+BASE="$(dirname $(cd $(dirname "$0"); pwd))"
+cd "$BASE"
+
+release_version=$1
+if [ -n "${2:-}" ]
+then
+    release_date=$(LANG=C date +"%B %-d, %Y" -d "$2")
+else
+    release_date="Month DD, YYYY"
+fi
+
+require_clean_work_tree () {
+    git rev-parse --verify HEAD >/dev/null || exit 1
+    git update-index -q --ignore-submodules --refresh
+    err=0
+
+    if ! git diff-files --quiet --ignore-submodules
+    then
+        echo >&2 "Cannot $1: You have unstaged changes."
+        err=1
+    fi
+
+    if ! git diff-index --cached --quiet --ignore-submodules HEAD --
+    then
+        if [ $err = 0 ]
+        then
+            echo >&2 "Cannot $1: Your index contains uncommitted changes."
+        else
+            echo >&2 "Additionally, your index contains uncommitted changes."
+        fi
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        # if there is a 2nd argument print it
+        test -n "${2+1}" && echo >&2 "$2"
+        exit 1
+    fi
+}
+
+require_clean_work_tree "set version"
+
+if branch=$(git symbolic-ref --short -q HEAD) && [ $branch = 'master' ]
+then
+    echo "Setting version ${release_version}"
+else
+    echo >&2 "Release is not possible because you are not on 'master' branch ($branch)"
+    exit 1
+fi
+
+sed -i -e "3s/^%.*/% ${release_date}/; 1s/| Version .*/| Version ${release_version}/" doc/barman.1.d/00-header.md doc/barman.5.d/00-header.md
+sed -i -e "3s/^%.*/% ${release_date} (${release_version})/" doc/manual/00-head.en.md
+sed -i -e "s/__version__ = .*/__version__ = '${release_version}'/" barman/version.py
+
+make -C doc
+
+git add doc/barman.1.d/00-header.md doc/barman.5.d/00-header.md doc/manual/00-head.en.md barman/version.py doc/barman.1 doc/barman.5
+git commit -sm "Version set to ${release_version}"
+
+echo "Version set to ${release_version}"
