EnterpriseDB/barman

commit 4b493022be79624da8deef95b88dd57b3ab5c12b

Preserve PostgreSQL connection status in aliveness check
In some cases, PostgreSQL was terminating the connection for the
start backup with the following error message:
"FATAL:  terminating connection due to idle-in-transaction timeout".
This was caused by the "SELECT 1" query that checks that a connection
is alive.

Fixes GH149

Signed-off-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
Signed-off-by: Gabriele Bartolini <gabriele.bartolini@2ndQuadrant.it>

barman/postgres.py
@@ -26,7 +26,7 @@ from abc import ABCMeta
 import psycopg2
 from psycopg2.errorcodes import (DUPLICATE_OBJECT, OBJECT_IN_USE,
                                  UNDEFINED_OBJECT)
-from psycopg2.extensions import STATUS_IN_TRANSACTION
+from psycopg2.extensions import STATUS_IN_TRANSACTION, STATUS_READY
 from psycopg2.extras import DictCursor, NamedTupleCursor
 
 from barman.exceptions import (ConninfoException, PostgresAppNameError,
@@ -172,7 +172,9 @@ class PostgreSQL(with_metaclass(ABCMeta, RemoteStatusMixin)):
 
         # Check if the connection works by running 'SELECT 1'
         cursor = None
+        initial_status = None
         try:
+            initial_status = self._conn.status
             cursor = self._conn.cursor()
             cursor.execute(self.CHECK_QUERY)
         except psycopg2.DatabaseError:
@@ -185,6 +187,10 @@ class PostgreSQL(with_metaclass(ABCMeta, RemoteStatusMixin)):
             return False
         finally:
             if cursor:
+                # Rollback if initial status was IDLE because the CHECK QUERY
+                # has started a new transaction.
+                if initial_status == STATUS_READY:
+                    self._conn.rollback()
                 cursor.close()
 
         return True

commit 05be218d0c0ac0aa7d4e951dbd2e94576400206f

Fix PostgreSQL connection aliveness check
The previous code was raising an error when the PostgreSQL connection
was unexpectedly dropped:

    ERROR: Backup failed issuing stop backup command.
    DETAILS: 'NoneType' object has no attribute 'rollback'

Now the rollback is only issued when the connection is healty.

Regression introduced in 4b493022be79624da8deef95b88dd57b3ab5c12b

Closes: GH-239

Signed-off-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
Signed-off-by: Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>

barman/postgres.py
@@ -177,6 +177,10 @@ class PostgreSQL(with_metaclass(ABCMeta, RemoteStatusMixin)):
             initial_status = self._conn.status
             cursor = self._conn.cursor()
             cursor.execute(self.CHECK_QUERY)
+            # Rollback if initial status was IDLE because the CHECK QUERY
+            # has started a new transaction.
+            if initial_status == STATUS_READY:
+                self._conn.rollback()
         except psycopg2.DatabaseError:
             # Connection is broken, so we need to reconnect
             self.close()
@@ -187,10 +191,6 @@ class PostgreSQL(with_metaclass(ABCMeta, RemoteStatusMixin)):
             return False
         finally:
             if cursor:
-                # Rollback if initial status was IDLE because the CHECK QUERY
-                # has started a new transaction.
-                if initial_status == STATUS_READY:
-                    self._conn.rollback()
                 cursor.close()
 
         return True