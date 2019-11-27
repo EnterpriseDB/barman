4ad833df21f0cf61f0d5d496a88176178d1ef3b6
Gabriele Bartolini <gabriele.bartolini@2ndQuadrant.it>
2019-11-27 18:34:12 +0100

    Add man pages for cloud utilities to setup.py
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@2ndQuadrant.it>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>

--- setup.py
+++ setup.py
@@ -71,6 +71,8 @@ setup(
     packages=find_packages(exclude=["tests"]),
     data_files=[
         ('share/man/man1', ['doc/barman.1',
+                            'doc/barman-cloud-backup.1',
+                            'doc/barman-cloud-wal-archive.1',
                             'doc/barman-wal-archive.1',
                             'doc/barman-wal-restore.1']),
         ('share/man/man5', ['doc/barman.5']),
