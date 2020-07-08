a60b80125b6a3a12651fd777e159175c52465188
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2020-07-08 11:11:20 +0200

    Add Python 3.8 compatibility to release metadata
    Signed-off-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     setup_requires=setup_requires,
     tests_require=[
