602821f9d3d1f03f44c77c492718c79e04473b1b
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2020-08-24 14:45:19 +0200

    Avoid any attempt to connect to PostgreSQL during tests
    Signed-off-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
    Signed-off-by: Giulio Calacoci <giulio.calacoci@2ndquadrant.it>

---
+++ tests/conftest.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2013-2020 2ndQuadrant Limited
+#
+# This file is part of Barman.
+#
+# Barman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Barman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Barman.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import psycopg2
+import pytest
+import mock
+
+
+@pytest.fixture(scope="session", autouse=True)
+def default_session_fixture(request):
+    """
+    Make sure that any real connection to Postgres results in an error
+
+    :type request: _pytest.python.SubRequest
+    :return:
+    """
+    logging.info("Patching barman.postgres.psycopg2.connect")
+    connect_patch = mock.patch('barman.postgres.psycopg2.connect')
+    connect_mock = connect_patch.__enter__()
+    connect_mock.side_effect = psycopg2.DatabaseError
+
+    def unpatch():
+        connect_patch.__exit__()
+        logging.info("Unpatching barman.postgres.psycopg2.connect")
+
+    request.addfinalizer(unpatch)
