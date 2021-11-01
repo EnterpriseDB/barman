262b319130af1d7979fc33af0454b63cef1d9b70
Michael Wallace <michael.wallace@enterprisedb.com>
2021-11-01 15:58:03 +0000

    Provide commands to check WAL archive destination is usable
    Adds commands to barman and barman cloud which check that a barman server
    or cloud location is safe to use as an archive destination for a new
    PostgreSQL server.
    
    A location is considered safe if either:
    
    1. There are no WAL files at all in the archive.
    2. All existing WAL files belong to an older timeline than that
       specified by the --timeline argument.
    
    A file is considered a WAL file if it passes the `is_any_xlog_file` check
    in `barman/xlog.py` so this applies to WAL files, history files, partial
    WAL files and backup labels.
    
    The commands added are:
    
    * barman check-wal-archive
    * barman-cloud-check-wal-archive
    
    The motivation for this patch is to provide a way that external
    orchestration tools can validate the WAL archive destination is safe for
    a newly provisioned PostgreSQL cluster, given such a cluster may use the
    exact same name as an old cluster.
    
    In such scenarios, any WAL files on the same or higher timeline as the
    WALs being written by the new cluster will cause any attempt to restore
    from a backup to fail.
    
    Reasons why external orchestration tooling may re-use the same cluster
    name and archive destination include (but are not limited to):
    
    * A new cluster is created via initdb with the same name as the old one.
      The sysid will be different but this does not affect the archive
      destination so any archived WALs relating to the older cluster will be
      present in the same location.
    * A cluster is restored from a base backup and uses the same name as the
      old cluster. The cluster has the same sysid and starts with a segment
      ID > 1 and timeline > 1. The same archive destination used by the old
      cluster will be used for the restored cluster.
    * A new cluster is started which happens to re-use the same name and
      archive destination.
    
    All of these cases lead to the situation where WAL archiving and backup
    is functioning normally *but* any attempts to restore from those backups
    will fail. This is dangerous for anyone relying on the databases managed
    by external orchestration/automation.
    
    The commands provided by this patch do not solve the problem alone
    because neither Barman nor PostgreSQL have the necessary context. The
    commands can, however, be added to external automation in order to
    catch archive safety issues at the provisioning stage.
    
    Closes #432

---
+++ barman/clients/cloud_check_wal_archive.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+# © Copyright EnterpriseDB UK Limited 2018-2021
+#
+# This file is part of Barman.
+#
+# Barman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Barman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Barman.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import logging
+
+import barman
+from barman.cloud import configure_logging, CloudBackupCatalog
+from barman.cloud_providers import get_cloud_interface
+from barman.exceptions import WalArchiveContentError
+from barman.utils import force_str, check_positive
+from barman.xlog import check_archive_usable
+
+
+def main(args=None):
+    """
+    The main script entry point
+
+    :param list[str] args: the raw arguments list. When not provided
+        it defaults to sys.args[1:]
+    """
+    config = parse_arguments(args)
+    configure_logging(config)
+
+    try:
+        cloud_interface = get_cloud_interface(config)
+        catalog = CloudBackupCatalog(cloud_interface, config.server_name)
+        wals = list(catalog.get_wal_paths().keys())
+        check_archive_usable(
+            wals,
+            timeline=config.timeline,
+        )
+    except WalArchiveContentError as err:
+        logging.error(
+            "WAL archive check failed for server %s: %s",
+            config.server_name,
+            force_str(err),
+        )
+        raise SystemExit(1)
+    except Exception as exc:
+        logging.error("Barman cloud WAL archive check exception: %s", force_str(exc))
+        logging.debug("Exception details:", exc_info=exc)
+        raise SystemExit(2)
+
+
+def parse_arguments(args=None):
+    """
+    Parse command line arguments
+
+    :return: The options parsed
+    """
+
+    parser = argparse.ArgumentParser(
+        description="Checks that the WAL archive on the specified cloud storage "
+        "can be safely used for a new PostgreSQL server.",
+        add_help=False,
+    )
+    parser.add_argument(
+        "destination_url",
+        help="URL of the cloud destination, such as a bucket in AWS S3."
+        " For example: `s3://bucket/path/to/folder`.",
+    )
+    parser.add_argument(
+        "server_name", help="the name of the server as configured in Barman."
+    )
+    parser.add_argument(
+        "-V", "--version", action="version", version="%%(prog)s %s" % barman.__version__
+    )
+    parser.add_argument("--help", action="help", help="show this help message and exit")
+    verbosity = parser.add_mutually_exclusive_group()
+    verbosity.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="increase output verbosity (e.g., -vv is more than -v)",
+    )
+    verbosity.add_argument(
+        "-q",
+        "--quiet",
+        action="count",
+        default=0,
+        help="decrease output verbosity (e.g., -qq is less than -q)",
+    )
+    parser.add_argument(
+        "-t",
+        "--test",
+        help="Test cloud connectivity and exit",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--timeline",
+        help="The earliest timeline whose WALs should cause the check to fail",
+        type=check_positive,
+    )
+    parser.add_argument(
+        "--cloud-provider",
+        help="The cloud provider to use as a storage backend",
+        choices=["aws-s3", "azure-blob-storage"],
+        default="aws-s3",
+    )
+    s3_arguments = parser.add_argument_group(
+        "Extra options for the aws-s3 cloud provider"
+    )
+    s3_arguments.add_argument(
+        "--endpoint-url",
+        help="Override default S3 endpoint URL with the given one",
+    )
+    s3_arguments.add_argument(
+        "-P",
+        "--profile",
+        help="profile name (e.g. INI section in AWS credentials file)",
+    )
+    return parser.parse_args(args=args)
+
+
+if __name__ == "__main__":
+    main()
