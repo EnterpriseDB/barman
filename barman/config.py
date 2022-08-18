3776e0690b81bc35c63e5a38b0e400ac33097a49
Michael Wallace <michael.wallace@enterprisedb.com>
2022-08-18 13:47:34 +0100

    [wip] Allow `backup_method = cloud`

--- barman/config.py
+++ barman/config.py
@@ -66,7 +66,7 @@ _SI_SUFFIX_RE = re.compile(r"""(\d+)\s*(k|Ki|M|Mi|G|Gi|T|Ti)?\s*$""")
 REUSE_BACKUP_VALUES = ("copy", "link", "off")
 
 # Possible copy methods for backups (must be all lowercase)
-BACKUP_METHOD_VALUES = ["rsync", "postgres", "local-rsync"]
+BACKUP_METHOD_VALUES = ["rsync", "postgres", "local-rsync", "cloud"]
 
 CREATE_SLOT_VALUES = ["manual", "auto"]
 
