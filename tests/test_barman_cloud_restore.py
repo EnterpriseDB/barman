462238dbe110b41c9cb7ba6ca32707da002af071
Michael Wallace <michael.wallace@enterprisedb.com>
2022-11-11 13:53:11 +0000

    [sq] Add test for restore/backup name

---
+++ tests/test_barman_cloud_restore.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# © Copyright EnterpriseDB UK Limited 2013-2022
+#
+# Client Utilities for Barman, Backup and Recovery Manager for PostgreSQL
+#
+# Barman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Barman is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Barman.  If not, see <http://www.gnu.org/licenses/>.
+
+import mock
+import pytest
+
+from barman.clients import cloud_restore
+
+
+class TestCloudRestore(object):
+    @pytest.mark.parametrize(
+        ("backup_id_arg", "expected_backup_id"),
+        (("20201110T120000", "20201110T120000"), ("backup name", "20201110T120000")),
+    )
+    @mock.patch("barman.clients.cloud_restore.CloudBackupDownloader")
+    @mock.patch("barman.clients.cloud_restore.CloudBackupCatalog")
+    @mock.patch("barman.clients.cloud_restore.get_cloud_interface")
+    def test_restore_calls_backup_downloader_with_parsed_id(
+        self,
+        _mock_cloud_interface_factory,
+        mock_catalog,
+        mock_downloader,
+        backup_id_arg,
+        expected_backup_id,
+    ):
+        """Verify that we downlaod the backup_id returned by parse_backup_id."""
+        # GIVEN a mock backup catalog where parse_backup_id will always resolve
+        # to the expected backup ID
+        mock_catalog.return_value.parse_backup_id.return_value = expected_backup_id
+
+        # WHEN barman-backup-restore is called with the backup_id_arg
+        recovery_dir = "/tmp/recover"
+        cloud_restore.main(
+            ["cloud_storage_url", "test_server", backup_id_arg, recovery_dir]
+        )
+
+        # THEN the backup downloader is called with the expected backup ID
+        mock_downloader.return_value.download_backup.assert_called_once_with(
+            expected_backup_id, recovery_dir, {}
+        )
