dcb3a286d47c36d79f765cea0fd7d4587012f39e
Michael Wallace <michael.wallace@enterprisedb.com>
2023-01-06 10:52:28 +0000

    [sq] Import print for cloud show fun

--- barman/clients/cloud_backup_show.py
+++ barman/clients/cloud_backup_show.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with Barman.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import print_function
+
 import json
 import logging
 from contextlib import closing
