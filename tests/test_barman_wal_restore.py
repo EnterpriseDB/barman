ca3a07ff64eec790dadd46caec5a106699ef6dfd
Gustavo William <gustavo.oliveira@enterprisedb.com>
2025-08-08 10:53:37 -0400

    Fix and add tests regarding chaging parallelism in barman-wal-restore
    Fixed all tests that were broken when changing the parallelism
    of barman-wal-restore to work with `multiprocessing`.
    
    Also added a bunch of tests to functions present in `walrestore.py`
    that were not tested before.
    
    References: BAR-865
    
    Signed-off-by: Gustavo William <gustavo.oliveira@enterprisedb.com>

--- tests/test_barman_wal_restore.py
+++ tests/test_barman_wal_restore.py
@@ -17,6 +17,7 @@
 # along with Barman.  If not, see <http://www.gnu.org/licenses/>.
 import os
 import subprocess
+from io import BytesIO
 
 import mock
 import pytest
@@ -171,67 +172,457 @@ class TestRemoteGetWal(object):
             "ERROR: Cannot open 'dummy_dest' (WAL_DEST) for writing: error\n"
         ) in err
 
+    @mock.patch("barman.clients.walrestore.get_server_config_minimal")
+    @mock.patch("barman.clients.walrestore.CompressionManager")
+    @mock.patch("barman.clients.walrestore.subprocess.Popen")
+    @mock.patch("barman.clients.walrestore.build_ssh_command")
+    def test_fetch_uncompressed_file(
+        self,
+        mock_build_command,
+        mock_popen,
+        mock_comp_manager,
+        _mock_get_server_config_minimal,
+    ):
+        """
+        Test fetching an uncompressed WAL file. The result of ``get-wal``
+        is simply written to the destination file.
+        """
+        # GIVEN the following parameters
+        config = mock.Mock()
+        wal_name = "000000010000000000000001"
+        dest_file = mock.Mock(wraps=BytesIO())
+
+        # Make sure no compression is detected on the WAL file
+        mock_comp_manager.return_value.identify_compression.return_value = None
+
+        # WHEN RemoteGetWal is called
+        walrestore.RemoteGetWal(config, wal_name, dest_file)
+
+        # THEN the ssh command is built and called, and the output is written to dest_file
+        mock_build_command.assert_called_once_with(config, wal_name)
+        mock_popen.assert_called_once_with(
+            mock_build_command.return_value, stdout=dest_file
+        )
+
+        # Make sure the file is rewound and closed
+        dest_file.seek.assert_called_once_with(0)
+        dest_file.close.assert_called_once()
+
+    @mock.patch("barman.clients.walrestore.get_server_config_minimal")
+    @mock.patch(
+        "barman.clients.walrestore.BytesIO", return_value=mock.Mock(read=BytesIO())
+    )
     @mock.patch("barman.clients.walrestore.CompressionManager")
     @mock.patch("barman.clients.walrestore.shutil")
     @mock.patch("barman.clients.walrestore.subprocess.Popen")
-    @mock.patch("barman.clients.walrestore.isinstance")
-    @mock.patch("barman.clients.walrestore.open")
-    def test_decompression(
+    @mock.patch("barman.clients.walrestore.build_ssh_command")
+    def test_fetch_compressed_file(
         self,
-        _mock_open,
-        mock_isinstance,
+        mock_build_command,
         mock_popen,
-        _mock_shutil,
-        mock_compression_manager,
-        tmpdir,
+        mock_shutil,
+        mock_comp_manager,
+        mock_bytesio,
+        _mock_get_server_config_minimal,
     ):
-        """Assert that decompression happens correctly when WALs come compressed"""
+        """
+        Test fetching a compressed WAL file. The result of ``get-wal`` is
+        decompressed and written to the destination file.
+        """
+        # GIVEN the following parameters
+        config = mock.Mock()
+        wal_name = "000000010000000000000001"
+        dest_file = mock.Mock(wraps=BytesIO())
 
-        # A config args object with some compression passed
-        config = mock.Mock(
-            compression="some compression",
-            user="barman",
-            barman_host="remote.barman.host",
-            config=None,
-            server_name="this-server",
+        # A compression is to be detected on the WAL file
+        mock_comp_manager.return_value.identify_compression.return_value = "gzip"
+
+        # WHEN RemoteGetWal is called
+        walrestore.RemoteGetWal(config, wal_name, dest_file)
+
+        # THEN the ssh command is built and called, and the output is written to dest_file
+        mock_build_command.assert_called_once_with(config, wal_name)
+        mock_popen.assert_called_once_with(
+            mock_build_command.return_value, stdout=dest_file
         )
-        dest_file = tmpdir.join("test-dest").strpath
+        # Make sure the file is rewound
+        dest_file.seek.assert_called_with(0)
 
-        # Mock the compression manager to identify a compression on the WAL file and
-        # return a corresponding compressor
-        mock_compressor = mock.Mock()
-        mock_compression_manager.return_value.identify_compression.return_value = (
-            "some compression"
+        # AND THEN the file is decompressed using the appropariate compressor object
+        compressor = mock_comp_manager.return_value.get_compressor.return_value
+        # Decompression consists of
+        # 1) Getting the decompressed file-like object
+        compressor.decompress_in_mem.assert_called_once_with(dest_file)
+        dec_fileobj = compressor.decompress_in_mem.return_value
+        # 2) Copying the decompressed content to a BytesIO object
+        mock_bytesio.assert_called_with(dec_fileobj.read.return_value)
+        # 3) Erasing and rewinding the destination file
+        dest_file.truncate.assert_called_once_with(0)
+        dest_file.seek.assert_called_with(0)
+        # 4) Copying the decompressed content to the destination file
+        mock_shutil.copyfileobj.assert_called_once_with(
+            mock_bytesio.return_value, dest_file
         )
-        mock_compression_manager.return_value.get_compressor.return_value = (
-            mock_compressor
+
+        # Make sure the destination file is closed at the end
+        dest_file.close.assert_called_with()
+
+    @mock.patch("barman.clients.walrestore.get_server_config_minimal")
+    @mock.patch("barman.clients.walrestore.CompressionManager")
+    @mock.patch("barman.clients.walrestore.subprocess.Popen")
+    @mock.patch("barman.clients.walrestore.build_ssh_command")
+    @mock.patch("barman.clients.walrestore.open", return_value=BytesIO())
+    def test_dest_file_is_string(
+        self,
+        mock_open,
+        mock_build_command,
+        mock_popen,
+        mock_comp_manager,
+        _mock_get_server_config_minimal,
+    ):
+        """
+        Simple test to ensure that the destination file can also be a string.
+        In this case, the file is opened in write-binary mode at the beginning.
+        """
+        # GIVEN the following parameters
+        config = mock.Mock()
+        wal_name = "000000010000000000000001"
+        dest_file = "/path/to/destination/file"
+
+        # Make sure no compression is detected on the WAL file
+        mock_comp_manager.return_value.identify_compression.return_value = None
+
+        # WHEN RemoteGetWal is called
+        walrestore.RemoteGetWal(config, wal_name, dest_file)
+
+        # THEN make sure the file was opened at the beginning
+        mock_open.assert_called_once_with(dest_file, "wb+")
+
+    @pytest.mark.parametrize("returncode", [0, 1, 255])
+    def test_returncode(self, returncode):
+        """
+        Test the :attr:`returncode` property of :class:`RemoteGetWal`.
+        It should return the return code of the underlying SSH Popen process.
+        """
+        # GIVEN a RemoteGetWal instance
+        # NOTE: we mock the __init__ method to avoid unnecessary
+        # as we don't need to initialize a complete instance to test this
+        walrestore.RemoteGetWal.__init__ = lambda self, *args, **kwargs: None
+        walrestore.RemoteGetWal.ssh_process = mock.Mock()
+        walrestore.RemoteGetWal.ssh_process.returncode = returncode
+
+        # WHEN we create an instance of RemoteGetWal
+        remote_get_wal = walrestore.RemoteGetWal(
+            config=mock.Mock(),
+            wal_name="000000010000000000000001",
+            dest_file=mock.Mock(),
         )
 
-        # The first return means that dest_file is instance of str
-        # The second means that mock_compressor is not instance of InternalCompressor
-        mock_isinstance.side_effect = [True, True]
+        # THEN its returncode should be the same as its ssh_process returncode
+        assert remote_get_wal.returncode == returncode
 
-        walrestore.RemoteGetWal(config, "000000010000000000000001", dest_file)
 
-        # Then the internal decompress method of the compressor should have been called
-        # to decompress the file directly to the destination
-        mock_compressor.decompress.assert_called_once()
+@mock.patch("barman.clients.walrestore.WorkerProcess")
+def test_spawn_additional_process(mock_worker_process):
+    """
+    Test that :func:`spawn_additional_process` spawn the correct processes
+    based on the provided additional files.
+    """
+    # GIVEN the following parameters
+    config = mock.Mock(spool_dir="/var/tmp/walrestore")
+    additional_files = [
+        "000000010000000000000001",
+        "000000010000000000000002",
+        "000000010000000000000003",
+    ]
 
-        # Reset the mocks
-        mock_compression_manager.reset_mock()
-        mock_compressor.reset_mock()
+    # WHEN spawn_additional_process is called
+    ret = walrestore.spawn_additional_process(config, additional_files)
 
-        # The first return means that dest_file is instance of str
-        # The second means that mock_compressor is not instance of InternalCompressor
-        mock_isinstance.side_effect = [True, False]
-
-        walrestore.RemoteGetWal(config, "000000010000000000000001", dest_file)
-
-        # Then no decompress method is called because it is an instance of CommandCompressor
-        # which invokes a subprocess to decompress the file
-        mock_compressor.decompress.assert_not_called()
-
-        # Assert that the subprocess was spanwed
-        mock_popen.assert_called_with(
-            ["some compression", "-d"], stdin=mock.ANY, stdout=mock.ANY
+    # THEN it should spawn a WorkerProcess for each additional file
+    for wal_name in additional_files:
+        spool_file_name = os.path.join(config.spool_dir, wal_name)
+        mock_worker_process.assert_any_call(
+            target=walrestore.RemoteGetWal,
+            name="RemoteGetWal-%s" % wal_name,
+            args=(config, wal_name, spool_file_name),
+            kwargs={"is_worker_process": True},
+            spool_file_name=spool_file_name,
         )
+
+    # Ensure all processes were started
+    mock_worker_process.return_value.start.call_count == len(additional_files)
+
+    # Ensure the return value is a list of all worker processes spawned
+    assert ret == [mock_worker_process.return_value for _ in additional_files]
+
+
+@mock.patch("barman.clients.walrestore.execute_peek")
+@mock.patch("barman.clients.walrestore.os.path.exists", return_value=True)
+@pytest.mark.parametrize("parallel", [None, 3, 10])
+def test_peek_additional_files(_mock_path_exists, mock_execute_peek, parallel):
+    """
+    Test that :func:`peek_additional_files` works correctly. It should return a list of
+    additional WAL files to be fetched, excluding the first one (the main WAL file).
+    If `parallel` is None, it should return an empty list.
+    """
+    # GIVEN the following parameters
+    config = mock.Mock(
+        parallel=parallel,
+        wal_name="000000010000000000000001",
+        spool_dir="/var/tmp/walrestore",
+    )
+    # Mock the execute_peek function to return a proper list of WAL names
+    wal_names = (
+        [f"00000001000000000000000{i}" for i in range(1, parallel + 1)]
+        if parallel
+        else []
+    )
+    mock_execute_peek.return_value = wal_names.copy()
+
+    # WHEN peek_additional_files is called
+    ret = walrestore.peek_additional_files(config)
+
+    if not parallel:
+        # THEN it should return an empty list
+        assert ret == []
+    else:
+        # THEN it should call execute_peek with the provided config
+        mock_execute_peek.assert_called_once_with(config)
+        # AND return the list of additional files (excluding the first one)
+        assert ret == wal_names[1:]
+
+
+@mock.patch("barman.clients.walrestore.os.mkdir")
+@mock.patch("barman.clients.walrestore.os.path.exists", return_value=False)
+@mock.patch("barman.clients.walrestore.execute_peek")
+def test_peek_additional_files_spool_dir_is_created(
+    mock_execute_peek, mock_path_exists, mock_mkdir
+):
+    """
+    Test that :func:`peek_additional_files` creates the spool directory
+    if it does not exist.
+    """
+    # GIVEN the following parameters
+    config = mock.Mock(
+        parallel=3,
+        wal_name="000000010000000000000001",
+        spool_dir="/var/tmp/walrestore",
+    )
+    # Mock the execute_peek function to return a proper list of WAL names
+    mock_execute_peek.return_value = [
+        f"00000001000000000000000{i}" for i in range(1, config.parallel + 1)
+    ]
+
+    # WHEN peek_additional_files is called
+    walrestore.peek_additional_files(config)
+
+    # THEN it should make sure the spool directory exists at the beginning
+    mock_mkdir.assert_called_once_with(config.spool_dir)
+
+
+@pytest.mark.parametrize(
+    "config, wal_name, peek, expected_command",
+    [
+        (
+            mock.Mock(
+                barman_host="my.barman.host",
+                server_name="test_server",
+                user="barman",
+                port=None,
+                config=None,
+                test=None,
+                compression=None,
+                keep_compression=False,
+                partial=False,
+            ),
+            "000000010000000000000001",
+            None,
+            [
+                "ssh",
+                "-q",
+                "-T",
+                "barman@my.barman.host",
+                "barman",
+                "get-wal 'test_server' '000000010000000000000001'",
+            ],
+        ),
+        (
+            mock.Mock(
+                barman_host="my.barman.host",
+                server_name="test_server",
+                user="barman",
+                port="22",
+                config="/etc/barman.conf",
+                test=True,
+                compression=None,
+                keep_compression=False,
+                partial=False,
+            ),
+            "000000010000000000000001",
+            None,
+            [
+                "ssh",
+                "-p",
+                "22",
+                "-q",
+                "-T",
+                "barman@my.barman.host",
+                "barman",
+                "--config /etc/barman.conf",
+                "get-wal --test 'test_server' '000000010000000000000001'",
+            ],
+        ),
+        (
+            mock.Mock(
+                barman_host="my.barman.host",
+                server_name="test_server",
+                user="barman",
+                port=None,
+                config=None,
+                test=None,
+                compression="gzip",
+                keep_compression=False,
+                partial=False,
+            ),
+            "000000010000000000000001",
+            None,
+            [
+                "ssh",
+                "-q",
+                "-T",
+                "barman@my.barman.host",
+                "barman",
+                "get-wal --gzip 'test_server' '000000010000000000000001'",
+            ],
+        ),
+        (
+            mock.Mock(
+                barman_host="my.barman.host",
+                server_name="test_server",
+                user="barman",
+                port=None,
+                config=None,
+                test=None,
+                compression=None,
+                keep_compression=True,
+                partial=False,
+            ),
+            "000000010000000000000001",
+            None,
+            [
+                "ssh",
+                "-q",
+                "-T",
+                "barman@my.barman.host",
+                "barman",
+                "get-wal --keep-compression 'test_server' '000000010000000000000001'",
+            ],
+        ),
+        (
+            mock.Mock(
+                barman_host="my.barman.host",
+                server_name="test_server",
+                user="barman",
+                port=None,
+                config=None,
+                test=None,
+                compression=None,
+                keep_compression=False,
+                partial=True,
+            ),
+            "000000010000000000000001",
+            10,
+            [
+                "ssh",
+                "-q",
+                "-T",
+                "barman@my.barman.host",
+                "barman",
+                "get-wal --peek '10' --partial 'test_server' '000000010000000000000001'",
+            ],
+        ),
+    ],
+)
+def test_build_ssh_command(config, wal_name, peek, expected_command):
+    """Test the build_ssh_command function with different configurations"""
+    command = walrestore.build_ssh_command(config, wal_name, peek)
+    assert command == expected_command
+
+
+@mock.patch("barman.clients.walrestore.subprocess.Popen")
+@mock.patch("barman.clients.walrestore.build_ssh_command")
+def test_execute_peek(mock_build_ssh_command, mock_popen):
+    """
+    Test that :func:`execute_peek` works correctly. It should use the ssh ``get-wal``
+    command to peek at the WAL files and return a list of their names.
+    """
+    # GIVEN a configuration object
+    config = mock.Mock(wal_name="000000010000000000000001", parallel=3)
+    # Mock the popen call to return fake WAL names
+    mock_popen.return_value.communicate.return_value = (
+        b"000000010000000000000001\n000000010000000000000002\n000000010000000000000003",
+        None,
+    )
+
+    # WHEN execute_peek is called
+    ret = walrestore.execute_peek(config)
+
+    # THEN it should build the ssh command
+    mock_build_ssh_command.assert_called_once_with(
+        config, config.wal_name, config.parallel
+    )
+    # AND call subprocess.Popen with the built command
+    mock_popen.assert_called_once_with(
+        mock_build_ssh_command.return_value, stdout=subprocess.PIPE
+    )
+    mock_popen.return_value.communicate.assert_called_once()
+
+    # AND return a list of WAL names correctly
+    assert ret == [
+        "000000010000000000000001",
+        "000000010000000000000002",
+        "000000010000000000000003",
+    ]
+
+
+@mock.patch("barman.clients.walrestore.subprocess.Popen")
+@mock.patch("barman.clients.walrestore.build_ssh_command")
+@mock.patch("barman.clients.walrestore.exit_with_error")
+def test_execute_peek_failed(mock_exit_with_error, _mock_build_ssh_command, mock_popen):
+    # GIVEN a configuration object
+    config = mock.Mock(wal_name="000000010000000000000001", parallel=3)
+    # Mock the popen call to raise an exception
+    exception = subprocess.CalledProcessError(1, "get-wal command failed!")
+    mock_popen.return_value.communicate.side_effect = exception
+
+    # WHEN execute_peek is called
+    walrestore.execute_peek(config)
+
+    # THEN it should exit with an error
+    mock_exit_with_error.assert_called_once_with(
+        "Impossible to invoke remote get-wal --peek: %s" % exception
+    )
+
+
+@mock.patch("barman.clients.walrestore.os.path.exists")
+@mock.patch("barman.clients.walrestore.shutil.move")
+def test_try_deliver_from_spool(mock_move, mock_path_exists):
+    """
+    Test that :func:`try_deliver_from_spool` correctly delivers a WAL file
+    from the spool directory if it exists.
+    """
+    # GIVEN the following parameters
+    config = mock.Mock(
+        wal_name="000000010000000000000001", spool_dir="/var/tmp/walrestore"
+    )
+    dest_file = "/path/to/destination/file"
+
+    # Mock the existence of the spool file
+    with mock.patch("barman.clients.walrestore.os.path.exists", return_value=True):
+        spool_file = os.path.join(config.spool_dir, config.wal_name)
+        with pytest.raises(SystemExit):
+            # WHEN try_deliver_from_spool is called
+            walrestore.try_deliver_from_spool(config, dest_file)
+            # THEN it should move the spool file to the destination
+            mock_move.assert_called_once_with(spool_file, dest_file)
